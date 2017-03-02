944de1a9edd35bc431eaa0052ebf16b9dec87a80
nawawy <nawawy@users.noreply.github.com>
2017-03-02 21:03:16 +0200

    Update regfile.v

--- regfile.v
+++ regfile.v
@@ -1,10 +1,34 @@
-module regfile (
-  input [31:0] writeData;
-  input[4:0]  address;
-  input writeEnable;
-  output[31:0] readData;
-
-  
-  
-  endmodule
-)
+    // file: regfile.v
+    // author: @amrheshamgalal
+    
+    `timescale 1ns/1ns
+    
+    module regfile(register,writeEnable,writeData,readData);
+    input [31:0] register,writeData;
+    input writeEnable;
+    output reg [31:0] readData;
+    
+    reg [31:0] regMemory [31:0];
+    always@(register or writeEnable)
+    begin
+    
+    if(writeEnable) //if we are writing
+        begin 
+        readData <= {32{1'bx}};
+        if(register != {32{1'b0}})
+        regMemory[register] <= writeData;
+        else
+        regMemory[register] <= {32{1'b0}}; end
+    else //if we are reading
+    begin
+        if(register !== {32{1'bx}})
+            if(register == {32{1'b0}})
+             readData <=  {32{1'b0}};
+            else
+             readData <= regMemory[register];
+    end
+    
+    
+    
+    end
+    endmodule
