29381edbb67c2f5bea65ceafa67c45725286c171
Amr Galal <amrheshamwahba@aucegypt.edu>
2017-03-03 15:06:15 +0200

    Update regfile.v
    *Read and write working
    *s3at readData btb2a weird writing
    *No latches

--- regfile.v
+++ regfile.v
@@ -1,34 +1,34 @@
-    // file: regfile.v
+    // file: Regfile.v
     // author: @amrheshamgalal
     
     `timescale 1ns/1ns
     
-    module regfile(register,writeEnable,writeData,readData);
+    module Regfile(clk,rst,register,writeEnable,writeData,readData); //single port registerfile module
     input [31:0] register,writeData;
-    input writeEnable;
-    output reg [31:0] readData;
+    input writeEnable,clk,rst;
+    output reg [31:0] readData; 
     
-    reg [31:0] regMemory [31:0];
-    always@(register or writeEnable)
-    begin
+    reg [31:0] regMemory [31:0];                 //32 32-bit register memory
     
-    if(writeEnable) //if we are writing
-        begin 
-        readData <= {32{1'bx}};
-        if(register != {32{1'b0}})
-        regMemory[register] <= writeData;
+    
+    always @(*)                                 //combinational logic to read registers
+        if(register == {32{1'b0}})
+            readData =  {32{1'b0}};
         else
-        regMemory[register] <= {32{1'b0}}; end
-    else //if we are reading
-    begin
-        if(register !== {32{1'bx}})
-            if(register == {32{1'b0}})
-             readData <=  {32{1'b0}};
+            readData = regMemory[register];
+        
+    always@(posedge clk)            //sequential logic to write in registers
+    
+        if(!rst)
+        
+            if(writeEnable)                         //if we are writing
+                    if(register != {32{1'b0}})
+                        regMemory[register] <= writeData;
+                    else
+                        regMemory[register] <= {32{1'b0}};
             else
-             readData <= regMemory[register];
-    end
-    
-    
-    
-    end
+                regMemory[0] <= {32{1'b0}};
+        else
+            readData <= {32{1'b0}};
+
     endmodule
