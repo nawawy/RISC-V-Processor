f57f4f65f297ff7159fb76e231db511ada150549
kareefardi <karimmhany@gmail.com>
2017-03-04 17:17:33 +0200

    added mem, regfile fix attempt

--- mem.v
+++ mem.v
@@ -1,10 +1,31 @@
 module mem (
-  input [31:0] address, dataIN,
-  output [31:0] memout,
-  input clk
-)
-  
-  
-  
-  endmodule;
-)
+// mem module used to model inst and data memory in a single unit
+    input [31:0] address, dataIn,
+    input clk,
+    input rst,
+    input wEn,
+    output [31:0] memout
+);
+
+reg [31 : 0] ram [31 : 0];
+
+/*
+//initial block to read instruction from memory
+initial begin
+    $readmemh("instr.dat", ram);
+end
+*/
+//sequential logic to model writing to memory
+
+always @(posedge clk)
+    if (rst)
+        //reset all values stored in memory?
+        ram[1] <= {32{1'b0}};
+    else 
+        if (wEn)
+            ram[address] <= dataIn;
+
+//combinational logic to read from memory 
+assign memout = ram[address];
+
+endmodule
--- regfile.v
+++ regfile.v
@@ -6,17 +6,19 @@
     module Regfile(clk,rst,register,writeEnable,writeData,readData); //single port registerfile module
     input [31:0] register,writeData;
     input writeEnable,clk,rst;
-    output reg [31:0] readData; 
+    output [31:0] readData; 
     
     reg [31:0] regMemory [31:0];                 //32 32-bit register memory
     
-    
+    /*
     always @(*)                                 //combinational logic to read registers
         if(register == {32{1'b0}})
             readData =  {32{1'b0}};
         else
             readData = regMemory[register];
-        
+      */
+    assign readData = regMemory[register];
+    
     always@(posedge clk)            //sequential logic to write in registers
     
         if(!rst)
@@ -28,7 +30,7 @@
                         regMemory[register] <= {32{1'b0}};
             else
                 regMemory[0] <= {32{1'b0}};
-        else
-            readData <= {32{1'b0}};
+        // else
+            // readData <= {32{1'b0}};
 
     endmodule
