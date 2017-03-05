8c178b7c46599ba41bc37a8657da0e7704bd7355
nawawy <nawawy@users.noreply.github.com>
2017-03-06 00:55:06 +0200

    Update controlunit.v

--- controlunit.v
+++ controlunit.v
@@ -6,7 +6,8 @@
 module controlUnit();
     
     input [31:0] instr;
-
+    input clk;
+    
     output reg [4:0] reg_signals; // regsel, regEN, rs1, rs2
     output reg [3:0] mem_signals; // fetchEN, bytesel, memWrite
     output reg [8:0] alu_signals; // srca, srcb, aluctrl, aluEN
@@ -22,9 +23,41 @@ module controlUnit();
     wire [6:0] func7;
     assign func7 = instr[31:25];
     
-    always@(*)
+    
+    reg [4:0] state = 0, nextstate;
+    
+    localparam  
+            fetch  = 5'd0,
+            decode1  = 5'd1,
+            decode2  = 5'd2,
+            alu  = 5'd3,
+            pc  = 5'd4,
+            memWrite  = 5'd5,
+            memRead  = 5'd6;
+
+    
+    always@(posedge clk)
     begin
-        
+        state = nextstate;
+    end
+    
+    
+    always@(state, instr)
+    begin
+        case(state)
+            fetch: 
+                begin
+                
+                
+                end
+            decode1:
+                begin
+                
+                
+                end
+    
+    
+    
     
     
     
