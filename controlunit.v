89f00d9b200b688f63130853141478edcc9c8527
nawawy <nawawy@users.noreply.github.com>
2017-03-06 15:18:06 +0200

    Update controlunit.v

--- controlunit.v
+++ controlunit.v
@@ -7,6 +7,7 @@ module controlUnit();
     
     input [31:0] instr;
     input clk;
+    input rst;
     
     output reg [4:0] reg_signals; // regsel, regEN, rs1, rs2
     output reg [5:0] mem_signals; // IFen, fetchEN, bytesel, memWrite
@@ -28,7 +29,7 @@ module controlUnit();
     
     localparam  
             fetch  = 5'd0,
-            decode1_r  = 5'd1,
+            decode1_R  = 5'd1,
             decode1_I  = 5'd3,
             decode2  = 5'd4,
             RB_alu  = 5'd5,
@@ -41,7 +42,7 @@ module controlUnit();
             S_MEM  = 5'd12,
             PCplus4  = 5'd13,
             B_PC  = 5'd14,
-            J_PC  = 5'd15,
+            J_PC  = 5'd15;
     
     always@(posedge clk)
     begin
@@ -49,31 +50,50 @@ module controlUnit();
     end
     
     
-    always@(state, instr)
+    always@(state or instr)
     begin
         case(state)
             fetch: 
                 begin
-                    mem_signals = 1'b11xxx0;
-                    wb_signals = 3'bxx0;
-                    reg_signals = 5'bxx000;
-                    imm_signals = 4'bxxx0;
-                    pc_signals = 3'b000;
-                    alu_signals = 9'b00xxxxxx0;
+                    mem_signals = 6'b11xxx0; // IFen, fetchEN, bytesel, memWrite
+                    wb_signals = 3'bxx0;   // wbSEl, wbEN
+                    reg_signals = 5'bxx000;  // regsel, regEN, rs1, rs2
+                    imm_signals = 4'bxxx0;   // immsel, immEN
+                    pc_signals = 3'b000;    // branch, pcEN, JalEN
+                    alu_signals = 9'b00xxxxxx0;  // srca, srcb, aluctrl, aluEN
+                    if(opcode == )
+                        nextstate = decode1_I;
+                    else
+                        nextstate = decode1_R;
                     
-                
                 end
-            decode1:
+            decode1_R:
                 begin
+                    mem_signals = 6'b00xxx0; // IFen, fetchEN, bytesel, memWrite
+                    wb_signals = 3'bxx0;   // wbSEl, wbEN
+                    reg_signals = 5'b00010;  // regsel, regEN, rs1, rs2
+                    imm_signals = 4'bxxx0;   // immsel, immEN
+                    pc_signals = 3'b000;    // branch, pcEN, JalEN
+                    alu_signals = 9'bxxxxxxxx0;  // srca, srcb, aluctrl, aluEN
+                    if (opcode == )
+                        nextstate = AUIPC_alu;
+                    else
+                        nextstate = decode2;
+                end
+            decode1_I:
+                begin
+                    mem_signals = 6'b00xxx0; // IFen, fetchEN, bytesel, memWrite
+                    wb_signals = 3'bxx0;   // wbSEl, wbEN
+                    reg_signals = 5'b10010;  // regsel, regEN, rs1, rs2
+                    imm_signals[3:1] = ;
+                    imm_signals[0] = 1'b1;   // immsel, immEN
+                    pc_signals = 3'b000;    // branch, pcEN, JalEN
+                    alu_signals = 9'bxxxxxxxx0;  // srca, srcb, aluctrl, aluEN
                 
                 
                 end
-    
-    
-    
-    
-    
-    
+            
+        endcase
     end
 
 endmodule
