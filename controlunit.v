6dc8ced4e24eda488814c683b9695b1fd98cdd99
nawawy <nawawy@users.noreply.github.com>
2017-03-06 14:27:03 +0200

    Update controlunit.v

--- controlunit.v
+++ controlunit.v
@@ -9,7 +9,7 @@ module controlUnit();
     input clk;
     
     output reg [4:0] reg_signals; // regsel, regEN, rs1, rs2
-    output reg [3:0] mem_signals; // fetchEN, bytesel, memWrite
+    output reg [5:0] mem_signals; // IFen, fetchEN, bytesel, memWrite
     output reg [8:0] alu_signals; // srca, srcb, aluctrl, aluEN
     output reg [3:0] imm_signals; // immsel, immEN
     output reg [2:0] pc_signals;  // branch, pcEN, JalEN
@@ -28,13 +28,20 @@ module controlUnit();
     
     localparam  
             fetch  = 5'd0,
-            decode1  = 5'd1,
-            decode2  = 5'd2,
-            alu  = 5'd3,
-            pc  = 5'd4,
-            memWrite  = 5'd5,
-            memRead  = 5'd6;
-
+            decode1_r  = 5'd1,
+            decode1_I  = 5'd3,
+            decode2  = 5'd4,
+            RB_alu  = 5'd5,
+            AUIPC_alu  = 5'd6,
+            I_alu  = 5'd7,
+            RIA_WB  = 5'd8,
+            LD_WB  = 5'd9,
+            J_WB  = 5'd10,
+            LD_MEM  = 5'd11,
+            S_MEM  = 5'd12,
+            PCplus4  = 5'd13,
+            B_PC  = 5'd14,
+            J_PC  = 5'd15,
     
     always@(posedge clk)
     begin
@@ -47,7 +54,14 @@ module controlUnit();
         case(state)
             fetch: 
                 begin
-                
+                    mem_signals = 1'b11xxx0;
+                    wb_signals = 3'bxx0;
+                    reg_signals[1:0] = 2'b00;
+                    imm_signals = 4'bxxx0;
+                    pc_signals = 3'b000;
+                    reg_signals[2] = 1'b0;
+                    alu_signals = 9'b00xxxxxx0;
+                    
                 
                 end
             decode1:
