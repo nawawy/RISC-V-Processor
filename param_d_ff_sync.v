20b1b9e23af20799770a9ab51d5350a186277dd7
kareefardi <karimmhany@gmail.com>
2017-03-06 18:34:19 +0200

    param ff added

---
+++ param_d_ff_sync.v
@@ -0,0 +1,25 @@
+// file: param_d_ff_async.v
+// author: @kareefardi
+
+`timescale 1ns/1ns
+
+module param_d_ff_sync(d, clk, rst, q, en);
+    parameter n = 2;
+    input [n - 1 : 0] d;
+	input clk;
+	input rst;
+    input en;
+	output reg [n - 1 : 0] q;
+
+	always @(posedge clk) begin
+		if (rst) begin
+            q <= {n{1'b0}};
+		end
+		else begin
+            if (en)
+                q  <= d;
+              else
+                q <= q;
+		end
+	end
+endmodule
