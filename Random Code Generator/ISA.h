fdd84ea50c6bd79e7c37333c48adb7d8b1a23ab1
Amr Galal <amrheshamwahba@aucegypt.edu>
2017-03-11 00:35:10 +0200

    First final draft

--- Random Code Generator/ISA.h
+++ Random Code Generator/ISA.h
@@ -12,32 +12,26 @@
 class ISA
 {
     private:
-    struct regsfile{
-        bool full;
-        int data;
-    };
+
     vector<Instr> list;
     vector<Instr> pc0;
-    regsfile regs[32];
+    bool regs[32];
     bool mem[32];
     int pc;
+    string handlePC0(int);
+    string handleRest(int,int);
+    void Itype(string&,int&,int&,int,int,bool);
+    void shifts(string&,int&,int&,int,int);
+    void UI(string&,int&,int);
+    void jumps(string&, int&,int&,int,int,int,bool);
+    void branches(string&,int&,int&,int&,int,int);
+    void loads(string&);
+    void Rtype(string&,int&,int&,int);
     
 
     public:
         ISA();
         void getRandom(int,string*);
-        string handlePC0(int);
-        void Itype(string&,int&,int&,int,int,bool,int);
-        void shifts(string&,int&,int&,int,int,int);
-        void UI(string&,int&,int,bool);
-        void jumps(string&, int&,int&,int,int,int,bool);
         ~ISA();
 };
 #endif //RISCV32I_RANDOM_TESTCASES_GENERATOR_ISA_H
-
-/*UI should be completely done {LUI,AUIPC}
-jumps should be completely done
-Itype should be completely done
-shifts should be working but we have a test case not working (low probability fash5 tho enha ttl3)
-
-not sure about the destructor yet*/
