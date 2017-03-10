2ea2a3a79327d252786c4da2ad8614f5248da8b5
Amr Galal <amrheshamwahba@aucegypt.edu>
2017-03-11 00:34:09 +0200

    Removed format

--- Random Code Generator/Instr.h
+++ Random Code Generator/Instr.h
@@ -5,21 +5,17 @@
 #ifndef INSTR_H
 #define INSTR_H
 #include <string>
-
 using namespace std;
+
 class Instr
 {
     private:
-        string format;
-        string keyword;
+    string keyword;
+
     public:
-        Instr(string,string);
-        string getFormat();
-        string getKeyword();
-        ~Instr();
+    Instr(string keyword);
+    string getKeyword();
+    ~Instr();
 };
 
 #endif //RISCV32I_RANDOM_TESTCASES_GENERATOR_INSTR_H
-
-//Format isn't used for now, (maybe it won't be used at all)
-//Nothing to comment on, too basic
