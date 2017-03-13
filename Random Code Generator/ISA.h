7128d4d6c098c7014c7188b7ddc4acf4f126322e
Amr Galal <amrheshamwahba@aucegypt.edu>
2017-03-13 13:11:41 -0300

    Update ISA.h

--- Random Code Generator/ISA.h
+++ Random Code Generator/ISA.h
@@ -11,27 +11,30 @@
 #include "Instr.h"
 class ISA
 {
-    private:
+private:
 
-    vector<Instr> list;
-    vector<Instr> pc0;
-    bool regs[32];
-    bool mem[32];
-    int pc;
-    string handlePC0(int);
-    string handleRest(int,int);
-    void Itype(string&,int&,int&,int,int,bool);
-    void shifts(string&,int&,int&,int,int);
-    void UI(string&,int&,int);
-    void jumps(string&, int&,int&,int,int,int,bool);
-    void branches(string&,int&,int&,int&,int,int);
-    void loads(string&);
-    void Rtype(string&,int&,int&,int);
-    
+	vector<Instr> list;
+	vector<Instr> pc0;
+	vector<int>indxs;
+	vector<int>imms;
+	bool regs[32];
+	bool mem[32];
+	int pc;
+	string handlePC0(int);
+	string handleRest(int, int);
+	void Itype(string&, int&, int&, int, int, bool);
+	void shifts(string&, int&, int&, int, int);
+	void UI(string&, int&, int);
+	void jumps(string&, int&, int&, int, int, int, bool);
+	void branches(string&, int&, int&, int&, int, int);
+	void loads(string&,int&,int&,int);
+	void store(string&, int&, int&, int&);
+	void Rtype(string&, int&, int&, int);
 
-    public:
-        ISA();
-        void getRandom(int,string*);
-        ~ISA();
+
+public:
+	ISA();
+	void getRandom(int, string*);
+	~ISA();
 };
 #endif //RISCV32I_RANDOM_TESTCASES_GENERATOR_ISA_H
