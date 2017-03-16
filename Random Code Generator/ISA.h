a3e1d45ceeb99c3c2d1110d0f52b83853b4324ba
Amr Galal <amrheshamwahba@aucegypt.edu>
2017-03-16 20:31:34 -0300

    Update ISA.h

--- Random Code Generator/ISA.h
+++ Random Code Generator/ISA.h
@@ -8,15 +8,20 @@
 #include <vector>
 #include <time.h>
 #include <cstring>
+#include <string>
 #include "Instr.h"
 class ISA
 {
 private:
 
+	struct brch {
+		string name;
+		int addr;
+	};
+	brch branch[20];
 	vector<Instr> list;
 	vector<Instr> pc0;
-	vector<int>indxs;
-	vector<int>imms;
+	int index;
 	bool regs[32];
 	bool mem[128];
 	int pc;
@@ -27,9 +32,10 @@ private:
 	void UI(string&, int&, int);
 	void jumps(string&, int&, int&, int, int, int, bool);
 	void branches(string&, int&, int&, int&, int, int);
-	void loads(string&,int&,int&,int);
+	void loads(string&, int&, int&, int);
 	void store(string&, int&, int&, int&);
 	void Rtype(string&, int&, int&, int);
+	void fixBranches(string*);
 
 
 public:
