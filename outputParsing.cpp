d630c2d4eb541255c53265d475424214b3bc21d2
Amr Galal <amrheshamwahba@aucegypt.edu>
2017-03-18 09:27:20 -0300

    Update outputParsing.cpp

--- outputParsing.cpp
+++ outputParsing.cpp
@@ -22,10 +22,10 @@ void parse1()
 void parse2()
 {
 	string s; string temp;
+	getline(ourOut, s);					//instruction
 	for (int i = 0; i < 32; i++)
 	{
 		getline(ourOut, s);
-		getline(ourOut, temp);
 		for (int j = s.length() - 8; j < s.length(); j++)
 			regsOur[i] += s[j];
 	}
@@ -34,42 +34,49 @@ void parse2()
 int main()
 {
 	int count = 0; int index = 0;
-    ifstream instr;
+	ifstream instr;
 	ofstream compareOut;
-    instr.open("output.txt");
-	gmOut.open("g_result.txt");
+	instr.open("output.txt");
+	gmOut.open("g_res2.txt");
 	ourOut.open("d_res.txt");
+	compareOut.open("nawww.txt");
 	string empty = "";
 	string s;
 
-	for (int i = 0; i < 32; i++)
-	{
-		regsGM[i] = "";
-		regsOur[i] = "";
-	}
-
-    while(instr.peek() != EOF)
-        getline(instr,instructions[index++]);
+	while (instr.peek() != EOF)
+		getline(instr, instructions[index++]);
 
 	getline(gmOut, s);           //"Program Excusion"
+	for (int i = 0; i < 36; i++)
+		getline(ourOut, s);	
 	while (1)
 	{
+		for (int i = 0; i < 32; i++)
+		{
+			regsGM[i] = "";
+			regsOur[i] = "";
+		}
+
 		parse1();
 		parse2();
-        compareOut << "Case: " << ++count << "\n";
+		compareOut << "Case: " << ++count << "\n";
 		for (int i = 0; i < 32; i++)
 			if (regsGM[i] != regsOur[i])
-			{compareOut << instructions[count-1] << "\n"; break;}
+			{
+				compareOut << instructions[count - 1] << "\n";
+				break;
+			}
 
 		char a, b;
 		a = gmOut.peek();
 		b = ourOut.peek();
-		if (a == EOF || b == EOF)
+		if (a == EOF || b == EOF || count == 20)
 			break;
 	}
 
+	compareOut.close();
 	ourOut.close();
 	gmOut.close();
-    instr.close();
+	instr.close();
 	return 0;
 }
